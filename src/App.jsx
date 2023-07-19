9b8b150827881b0436d77b787c3ead270cf434fb
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-07-19 23:04:53 +0700

    Added error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import RegisterPage from "./pages/Register";
+import ErrorPage from "./pages/Error/404";
 import LandingPage from "./pages/Landing";
 import { Toaster } from "react-hot-toast";
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/home" element={<LandingPage />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/register" element={<RegisterPage />}></Route>
+          <Route path="*" element={<ErrorPage />}></Route>
         </Routes>
       </BrowserRouter>
     </>

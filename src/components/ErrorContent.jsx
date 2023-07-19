680050e7099e250a5d3d0f7fa77f0a96c98a3b25
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-07-19 23:27:28 +0700

    Enlargened gap between button and text

--- src/components/ErrorContent.jsx
+++ src/components/ErrorContent.jsx
@@ -9,7 +9,7 @@ function ErrorContent() {
           404
         </p>
         <p className="text-lg text-white">Page Not Found!</p>
-        <div className="mt-5 lg:mt-7 text-center">
+        <div className="mt-7 lg:mt-7 text-center">
           <a
             href="/home"
             className="px-3 py-1 sm:px-5 text-md sm:text-lg text-white bg-gradient-to-b from-[#C44054] to-[#DB77A5] rounded-lg hover:from-pink-500 hover:to-yellow-500 hover:scale-105 duration-300"

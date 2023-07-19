ceee0b12dede7b36da757d39e4d06a08e6f0c904
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-07-19 23:22:43 +0700

    Modified tailwind.config.js and added custom fonts, screen sizes, and sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+    },
+    extend: {
+      fontFamily: {
+        dm: ["DM Sans"],
+      },
+      width: {
+        128: "32rem",
+      },
+    },
   },
   plugins: [],
 };

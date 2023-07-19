de578066f8a91cb645978a38938b3287ca2a1917
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-07-19 23:00:50 +0700

    Installed react-hot-toast and added it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/Landing";
+import { Toaster } from "react-hot-toast";
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Toaster />
         <Routes>
           <Route index element={<LandingPage />}></Route>
           <Route path="/home" element={<LandingPage />}></Route>

116f641a13fd536eb843aae53a57031bed34d3ea
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-07-19 23:04:15 +0700

    Added ui commons (Logo)

---
+++ src/components/UI/LogoCol.jsx
@@ -0,0 +1,10 @@
+function Logo() {
+  return (
+    <div className="flex flex-col justify-center items-center -mt-20 mb-10 lg:mt-0 lg:mb-0 xl:-mr-10">
+      <img src="assets\Vector.png" className="w-36 h-36 md:w-36 lg:w-96 lg:h-96"></img>
+      <img src="assets\Logo.png" className="w-auto h-12 lg:h-16"></img>
+    </div>
+  );
+}
+
+export default Logo;

ad98d3802ca5bfb2489420547d8a63902a91a33e
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-07-19 23:21:38 +0700

    Added button in error page to navigate back home

--- src/components/ErrorContent.jsx
+++ src/components/ErrorContent.jsx
@@ -4,7 +4,20 @@ function ErrorContent() {
   return (
     <>
       <Logo />
-      <p className="text-5xl text-white tracking-widest font-bold mb-10 mt-4">404</p>
+      <div className="flex flex-col justify-center items-center">
+        <p className="text-9xl text-white tracking-widest font-bold mb-3">
+          404
+        </p>
+        <p className="text-lg text-white">Page Not Found!</p>
+        <div className="mt-5 lg:mt-7 text-center">
+          <a
+            href="/home"
+            className="px-3 py-1 sm:px-5 text-md sm:text-lg text-white bg-gradient-to-b from-[#C44054] to-[#DB77A5] rounded-lg hover:from-pink-500 hover:to-yellow-500 hover:scale-105 duration-300"
+          >
+            Go Home
+          </a>
+        </div>
+      </div>
     </>
   );
 }

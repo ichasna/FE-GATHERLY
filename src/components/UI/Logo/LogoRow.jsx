82652d261c9e59b8dc86dd3aa725e58b9da52f00
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-07-20 13:04:13 +0700

    Changed button styles on hover and made logo navigate to home page on click

--- src/components/UI/Logo/LogoRow.jsx
+++ src/components/UI/Logo/LogoRow.jsx
@@ -1,10 +1,14 @@
 function Logo() {
-    return (
-      <div className="fixed left-0 top-0 p-8">
-        <img src="assets\Gatherly.png" className="h-auto w-36 md:w-40 lg:w-48"></img>
-      </div>
-    );
-  }
-  
-  export default Logo;
-  
+  return (
+    <div className="fixed left-0 top-0 p-8">
+      <a href="/home">
+        <img
+          src="assets\Gatherly.png"
+          className="h-auto w-36 md:w-40 lg:w-48"
+        ></img>
+      </a>
+    </div>
+  );
+}
+
+export default Logo;

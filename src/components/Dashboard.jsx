4624dbb607d98905b4d4091ed7874c2ce73d64c1
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-08-11 00:31:41 +0700

    Fixed more layout for dashboard page and components. TODO: Fix component overlapping when screen shrinks

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,10 +4,10 @@ import Navbar from "./UI/Commons/Navbar";
 function Dashboard() {
   return (
     <>
-      <div className="flex justify-center bg-blue-200 col-span-2">
+      <div className="flex justify-center items-center bg-blue-200 h-full col-span-2">
         <Navbar />
       </div>
-      <div className="flex justify-center bg-purple-200 col-span-10">
+      <div className="flex justify-center items-center bg-purple-200 h-full col-span-10">
         <Index />
       </div>
     </>

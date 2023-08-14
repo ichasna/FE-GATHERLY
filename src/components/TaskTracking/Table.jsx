d6d58ed842ea094af444be129237de335b9f83b0
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-08-14 23:43:34 +0700

    Made tablerow tasks into map

--- src/components/TaskTracking/Table.jsx
+++ src/components/TaskTracking/Table.jsx
@@ -3,35 +3,73 @@ import TableRow from "./TableRow";
 function Table() {
   const dummyData = [
     {
-      task: "Create an event plan and define roles",
-      dueDate: "15/05/2023",
-      PIC: "Amira Nisrina",
-      description: ""
-    }
+      title: "Planning",
+      data: [
+        {
+          task: "Create an event plan and define roles",
+          dueDate: "15/05/2023",
+          PIC: "Amira Nisrina",
+          description: "",
+        },
+      ],
+    },
+    {
+      title: "Execution",
+      data: [
+        {
+          task: "Create an event plan",
+          dueDate: "20/05/2023",
+          PIC: "Daffa",
+          description: "",
+        },
+      ],
+    },
+    {
+      title: "Post Event",
+      data: [
+        {
+          task: "Define roles",
+          dueDate: "21/05/2023",
+          PIC: "Rangga",
+          description: "",
+        },
+      ],
+    },
   ];
   return (
     <div className="rounded-lg overflow-hidden border-2 border-[#404040]">
       <table className="table-fixed w-full">
         <thead>
           <tr>
-            <th className="p-2 lg:p-3 border-r border-[#404040] col-span-4" colSpan={4}>
-              <p className="text-white text-[12px] lg:text-[15px] font-light">Task</p>
+            <th
+              className="p-2 lg:p-3 border-r border-[#404040] col-span-4"
+              colSpan={4}
+            >
+              <p className="text-white text-[12px] lg:text-[15px] font-light">
+                Task
+              </p>
             </th>
             <th className="p-2 lg:p-3  border-r border-[#404040]" colSpan={2}>
-              <p className="text-white text-[12px] lg:text-[15px] font-light">Due Date</p>
+              <p className="text-white text-[12px] lg:text-[15px] font-light">
+                Due Date
+              </p>
             </th>
             <th className="p-2 lg:p-3  border-r border-[#404040]" colSpan={2}>
-              <p className="text-white text-[12px] lg:text-[15px] font-light">PIC</p>
+              <p className="text-white text-[12px] lg:text-[15px] font-light">
+                PIC
+              </p>
             </th>
             <th className="p-2 lg:p-3  border-r border-[#404040]" colSpan={4}>
-              <p className="text-white text-[12px] lg:text-[15px] font-light">Description</p>
+              <p className="text-white text-[12px] lg:text-[15px] font-light">
+                Description
+              </p>
             </th>
           </tr>
         </thead>
         <tbody>
-          <TableRow title="Planning" data={dummyData} />
-          <TableRow title="Execution" data={dummyData} />
-          <TableRow title="Post Event" data={dummyData} />
+          {dummyData.map((data) => (
+            <TableRow title={data.title} data={data.data} />
+          ))}
         </tbody>
       </table>
     </div>

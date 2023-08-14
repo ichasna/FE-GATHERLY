058c48997a8eaeb02a999f71b1e7e0ba00733dda
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-08-15 00:30:48 +0700

    Changed padding of navbar wrapper div

--- src/components/TaskTracking.jsx
+++ src/components/TaskTracking.jsx
@@ -4,7 +4,7 @@ import Navbar from "./UI/Commons/Navbar";
 function TaskTracking() {
   return (
     <>
-      <div className="hidden lg:flex lg:justify-center lg:items-center py-4 h-full">
+      <div className="hidden lg:flex lg:justify-center lg:items-center p-4 h-full">
         <Navbar />
       </div>
       <div className="h-full w-full p-4 flex justify-center items-center">
